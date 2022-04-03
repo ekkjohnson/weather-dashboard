95ada2a37f94d441fbdb97fd440d51d8880abedd
Elizabeth <ekkjohnson@gmail.com>
2022-04-03 12:24:32 -0500

    js fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,37 @@ console.log(searchHistory);
 //display weather 
 
 //call
+function getCityWeather() {
+    var cityFinderUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},US&limit=1&appid=${APIKey}`;
+    fetch(cityFinderUrl).then(function (res) {
+      if (res.status !== 200) {
+        console.log("fetch found nothing!");
+        return;
+      } else {
+        res.json().then(function (data) {
+          lat_lon.push(data[0].lat.toFixed(2));
+          lat_lon.push(data[0].lon.toFixed(2));
+          var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat_lon[0]}&lon=${lat_lon[1]}&units=imperial&exclude=hourly,minutely&appid=${APIKey}`;
+          fetch(weatherUrl)
+            .then(function (res) {
+              if (res.status !== 200) {
+                console.log("fetch found nothing!");
+                return;
+              }
+              res.json().then(function (data) {
+                for (let i = 0; i < 6; i++) {
+                  forecast.push(data.daily[i]);
+                }
+                currentWeather();
+              });
+            })
+            .catch(function (err) {
+              console.error(err);
+            });
+        });
+      }
+    });
+}
 var formSumbit= function(event){
     event.preventDefault();
     var city = cityInput.value.trim();

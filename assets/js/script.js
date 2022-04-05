8488ac5c6fc9c9c49fbb4974c2507a98b7938b09
Elizabeth <ekkjohnson@gmail.com>
2022-04-04 21:28:37 -0500

    reversion

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,100 @@
 //search history
 // var city;
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
-
 const APIkey = "371075399ab3c5eb96abdb927a36cf0f";
-var day = moment().format('L');
-var historySearch= [];
 
-// function for current weather in input field city
-function currentWeather(city) {
+var cityInput = document.getElementById("cityInput");
+var searchBtn = document.getElementById("searchBtn");
+var currentWeatherEL = document.getElementById("current-container")
+var forecast = document.getElementById("forecast-container")
+var history = document.getElementById("history");
+var weather = 'https://api.openweathermap.org/data/2.5/onecall?'
 
-    var URL  = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
-//calls api
-    $.ajax({
-        url: URL,
-        method: "GET"
-    }).then(function(cityWeather) {
-        console.log(cityWeather);
+var rest = "&units=imperial&limit=1&appid="
+var displayWeather=function(weather, citySearch){
+    currentWeather.textContent="";
+    cityInput.textContent=citySearch;
+    console.log(weather);
+
+}
+
+// display weather 
+function getCityCoordinates (city) { 
+    var cityCoordinates = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`
+    
+ 
+    fetch(cityCoordinates)
+    .then(function(response){
+        return response.json();
+        })
         
-        $("#weatherContent").css("display", "block");
-        $("#cityInfo").empty();
-        //gets icons
-        var iconCode = cityWeather.weather[0].icon;
-        var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
-        var selectCity = $(`
-        <h2 id="selectCity">
-            ${cityWeather.name} ${day} <img src="${iconURL}" alt="${cityWeather.weather[0].description}" />
-        </h2>
-        <p>Temperature: ${cityWeather.main.temp} °F</p>
-        <p>Humidity: ${cityWeather.main.humidity}\%</p>
-        <p>Wind: ${cityWeather.wind.speed} MPH</p>
-    `);
-
-    $("#cityInfo").append(selectCity);
-        
+        .then(function(data) {
+            console.log(data)
+            getWeather(data[0].lon,data[0].lat)
+            // getForecast(data[0].lon,data[0].lat)
     })
+ 
+ }
+
+//call
+
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    var city= cityInput.value.trim()
+    console.log(city)
+    getCityCoordinates(city)
+    cityInput.value=""
+})
+
+
+ function getWeather(lon, lat) {
+    var requestURL= `${weather}lat=${lat}&lon=${lon}&appid=${APIkey}`;
+
+     fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data.current)
+        let currentWeather = {
+            temp: data.current.temp,
+            humidity:  data.current.humidity, 
+            wind: data.current.wind,
+            uvi: data.current.uvi
+        }
+        console.log( currentWeather)
+      
+        currentForecast(currentWeather)
+        // weatherData={data}
+    })
+ }
+
+
+// var currentDay=document.createElement("span")
+// currentDay.textContent="("+ moment(currentWeather.dt.value).format ("MMM D, YYYY") + ") ";
+citySearch.appendChild(currentDay);
+
+function currentForecast(current) {
+
+    // removeAllChildNodes(currentWeather);
+    // removeAllChildNodes(forecast)
+    // var iconUrl = `http://openweathermap.org/img/wn/${forecast[0].weather[0].icon}@2x.png`;
+    // var Icon;
+    // fetch(iconUrl).then(function (res) {
+    //   Icon = res.url;
+    //   return Icon;
+    // });
+    // var card = document.createElement("div");
+    // currentContainer.appendChild(card);
+    // card.setAttribute("class", "currentWeatherCard");
+    // var cityEl = document.createElement("h2");
+
+    // var date = document.createElement("h2");
+    // var icon = document.createElement("img");
+    var tempEl = document.createElement("h2");
+    tempEl.textContent = current.temp; 
+    currentWeatherEL.appendChild(tempEl)
+    // var wind = document.createElement("h2");
+    // var humidity = document.createElement("h2");
+    // var UV= document.createElement("h2");
 }

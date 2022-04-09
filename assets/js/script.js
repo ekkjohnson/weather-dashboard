d92322e800de26cadd1501738ca7cc167320f98b
Elizabeth <ekkjohnson@gmail.com>
2022-04-09 15:15:14 -0500

    revision

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,126 +12,37 @@
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const APIkey = "371075399ab3c5eb96abdb927a36cf0f";
 // var currentDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-var cityInput = document.getElementById("cityInput");
-var searchBtn = document.getElementById("searchBtn");
-var currentWeatherEL = document.getElementById("current-container")
-var forecast = document.getElementById("forecast-container")
-var history = document.getElementById("history");
-var weather = 'https://api.openweathermap.org/data/2.5/onecall?'
+var card1 = document.getElementById('card-body1')
+var searchInput= document.getElementById('searchCityInput')
+var searchButton= document.getElementById('search-btn')
+var searchList= document.getElementById('searchHistory')
+var card2= document.getElementById('card2')
 
-var rest = "&units=imperial&limit=1&appid="
-var displayWeather=function(weather, citySearch){
-    currentWeatherEL.textContent="";
-    cityInput.textContent=citySearch;
-    console.log(weather);
+//array to push searched cities into- I realized I name two values searchHistory and easily change this to search Mistory
+let searchMistory = [];
 
-}
-
-// call lon and lat
-function getCityCoordinates (city) { 
-    var cityCoordinates = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`
+//event listener function calling getCoordinates from the city
+searchButton.addEventListener('click', function(event) {
+    card1.innerHTML="";
+    event.preventDefault()
+    getCoordinates(searchInput.value)
     
- 
-    fetch(cityCoordinates)
-    .then(function(response){
-        return response.json();
-        })
-        
-        .then(function(data) {
-            console.log(data)
-            getWeather(data[0].lon,data[0].lat)
-            // getForecast(data[0].lon,data[0].lat)
-    })
- 
- }
-
-
-//click event
-searchBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    var city= cityInput.value.trim()
-    console.log(city)
-    getCityCoordinates(city)
-    cityInput.value=""
-})
-
-//use lon and lat to call weather api
- function getWeather(lon, lat) {
-    var requestURL= `${weather}lat=${lat}&lon=${lon}&appid=${APIkey}`;
-
-     fetch(requestURL)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data.current)
-        let currentWeather = {
-            temp: data.current.temp,
-            humidity:  data.current.humidity, 
-            wind: data.current.wind_speed,
-            uvi: data.current.uvi
-        }
-        console.log( currentWeather)
-      
-        currentForecast(currentWeather)
-        // weatherData={data}
-    })
- }
-
-
-// var currentDay=document.createElement("span")
-// currentDay.textContent="("+ moment(currentWeather.dt.value).format ("MMM D, YYYY") + ") ";
-// citySearch.appendChild(currentDay);
-
-function currentForecast(current) {
-
-    // removeAllChildNodes(currentWeather);
-    // removeAllChildNodes(forecast)
-    // var iconUrl = `http://openweathermap.org/img/wn/${forecast[0].weather[0].icon}@2x.png`;
-    // var Icon;
-    // fetch(iconUrl).then(function (res) {
-    //   Icon = res.url;
-    //   return Icon;
-    // });
-//variables to display
-    var tempEl = document.createElement("h2");
-    tempEl.textContent = current.temp; 
-    currentWeatherEL.appendChild(tempEl)
-    var humidityEl = document.createElement("h2");
-    humidityEl.textContent = current.humidity; 
-    currentWeatherEL.appendChild(humidityEl)
-    var uviEl = document.createElement("h2");
-    uviEl.textContent = current.uvi; 
-    currentWeatherEL.appendChild(uviEl)
-    var windEl = document.createElement("h2");
-    windEl.textContent = current.wind; 
-    currentWeatherEL.appendChild(windEl)
+    //Again, my button variable was renamed butto for button without the last letter. This stores the searched cities as a button
+    var butto = document.createElement("button")
+    butto.textContent= searchInput.value
     
-}
-function displayForecastData (data) {
-    // console.log(data.daily)
-    for (let i = 1; i < 6; i++) {
-        var cityInfo = {
-            date: futureResponse.daily[i].dt,
-            icon: futureResponse.daily[i].weather[0].icon,
-            temp: futureResponse.daily[i].temp.day,
-            humidity: futureResponse.daily[i].humidity
-
-            
+    //adding search history buttons to list element
+    searchList.append(butto)
+    
+    //event listener function to add searched cities to local storage
+    butto.addEventListener('click', function (event) {
+        card1.innerHTML="";
+        event.preventDefault()
+        getCoordinates(butto.textContent)
+        displayCityName(butto.textContent)
+    })
+    searchMistory.push(searchInput.value)
+        for (var i = 0; i < searchMistory.length; i++) {
+            localStorage.setItem("cities",searchMistory)
         }
-        var tempEl = document.createElement("h2");
-    tempEl.textContent = data.daily[i].temp; 
-    ForecastWeatherEL.appendChild(tempEl)
-    var humidityEl = document.createElement("h2");
-    humidityEl.textContent = data.daily[i].humidity; 
-    ForecastWeatherEL.appendChild(humidityEl)
-    var uviEl = document.createElement("h2");
-    uviEl.textContent = data.daily[i].uvi; 
-    ForecastWeatherEL.appendChild(uviEl)
-    var windEl = document.createElement("h2");
-    windEl.textContent = data.daily[i].wind; 
-    ForecastWeatherEL.appendChild(windEl)
-
-         
-     }
-    }
+    })
